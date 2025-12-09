84fa640c8d4437a56c2d4bde1744e36eecd07154
Keyu Ren <rendarenkeyu@126.com>
2025-12-09 20:08:12 +0000

    Fix air quality attribute implementation

--- homeassistant/components/air_quality/__init__.py
+++ homeassistant/components/air_quality/__init__.py
@@ -19,7 +19,7 @@ from .const import DOMAIN
 
 _LOGGER: Final = logging.getLogger(__name__)
 
-DATA_COMPONENT: HassKey[EntityComponent["AirQualityEntity"]] = HassKey(DOMAIN)
+DATA_COMPONENT: HassKey[EntityComponent[AirQualityEntity]] = HassKey(DOMAIN)
 ENTITY_ID_FORMAT: Final = DOMAIN + ".{}"
 PLATFORM_SCHEMA = cv.PLATFORM_SCHEMA
 PLATFORM_SCHEMA_BASE = cv.PLATFORM_SCHEMA_BASE
